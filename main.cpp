07bef5e5d9f681af128b8e39febcc8153918bae7
paolo <paolo.valcepina@gmail.com>
2017-04-26 20:01:55 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Lorenzo Nuti and Paolo Valcepina on 13/03/17.
+//
+
+#include <SFML/Graphics.hpp>
+
+#include "Event.h"
+#include "Window.h"
+#include "Background.h"
+#include "MainCharacter.h"
+
+int main() {
+    srand((unsigned int) (time(NULL)));
+
+    Window *window = Window::getInstance();
+    Background *background = Background::getInstance();
+    MainCharacter *character = MainCharacter::getInstance();
+
+    window->updateEvent += background;
+    window->updateEvent += character;
+    window->drawEvent += background;
+    window->drawEvent += character;
+
+    window->gameLoop();
+    return 0;
+}
