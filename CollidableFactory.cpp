f2e673a6a3e304ceecc80f3f75cc0b5f685cb215
Lorenzo-N <lorenzonuti.9@gmail.com>
2017-07-05 16:34:32 +0200

    Chance

--- CollidableFactory.cpp
+++ CollidableFactory.cpp
@@ -14,9 +14,9 @@ Collidable *CollidableFactory::createCollidable(ModelGame &modelGame) {
     int p = rand() % 100;
     if (p < 20)
         return new Rocket(modelGame, Window::getWidth(), rand() % ((int) Window::getHeight() - 300) + 100);
-    else if (p < 50)
+    else if (p < 40)
         return new Skull(modelGame, Window::getWidth(), rand() % ((int) Window::getHeight() - 300) + 100);
-    else if (p < 80)
+    else if (p < 70)
         return new Star(modelGame, Window::getWidth(), rand() % ((int) Window::getHeight() - 300) + 100);
     else
         return new Laser(modelGame, Window::getWidth(), rand() % ((int) Window::getHeight() - 300) + 100);
