dfb6a643bd0413b90d6d4ca740965627e881b548
cyberpal83 <cyberpal83@gmail.com>
2017-07-10 12:37:48 +0200

    Reformatted test/CMakeLists.txt. Updated runAllTests.

--- test/runAllTests.cpp
+++ test/runAllTests.cpp
@@ -3,8 +3,22 @@
 //
 
 #include "gtest/gtest.h"
+#include "../MessageBox.h"
+#include "../Fonts.h"
+#include "../Audio.h"
 
 int main(int argc, char **argv) {
+    try {
+        Audio::loadSounds();
+        Images::loadImages();
+        Fonts::loadFonts();
+    }
+    catch (const load_file_error &e) {
+        MessageBox mb("Error!");
+        mb.showMessage(std::string(e.what()) + ": \nPath: " + e.getPath() + "\nFilename: " + e.getFilename());
+        std::exit(EXIT_FAILURE);
+    }
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
